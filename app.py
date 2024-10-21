68219fd26b93e4076f5e97f38035fc24969e7c17
“Nahuel <nnahuell.gb@hotmail.com>
2024-10-21 19:51:52 -0300

    cambios

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -53,4 +54,5 @@ def delete_data(id):
     return jsonify({"error": "Failed to delete item"}), response.status_code
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
